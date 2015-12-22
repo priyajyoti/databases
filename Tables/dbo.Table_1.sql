af58bf20fe2382159c094d7eb6d5d812288a6704
priyajyoti <priya0623j@gmail.com>
2015-12-22 14:43:55 +0530

    Update dbo.Table_1.sql

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[Table_1]
 (
 [first_name] [nchar] (10) COLLATE Latin1_General_CI_AI NULL,
 [middle_name] [nchar] (10) COLLATE Latin1_General_CI_AI NULL,
+[middle_name_new] [nchar] (10) COLLATE Latin1_General_CI_AI NULL,
 [last_name] [nchar] (10) COLLATE Latin1_General_CI_AI NULL
 ) ON [PRIMARY]
 GO
